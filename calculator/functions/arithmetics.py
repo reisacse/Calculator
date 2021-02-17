e7a90d9d2ca5b1fb9983abce5b12f342f00e3820
Sebastian <basti.reisach@gmx.net>
2021-02-17 09:51:55 +0100

    first commit

---
+++ calculator/functions/arithmetics.py
@@ -0,0 +1,14 @@
+def summe(a, b):
+    return a + b
+
+
+def multiplikation(a, b):
+    return a * b
+
+
+def subtraktion(a, b):
+    return a - b
+
+
+if __name__ == '__main__':
+    print(summe(2, 2))
---
+++ tests/test.py
@@ -0,0 +1,3 @@
+import importlib
+x = importlib.util.find_spec('random')
+print("\nModule Specs: ", x)
